df0c960f18d4f113d30d2ff2663bd3dc1a161c88
Pete Goodall <pete@digicratic.com>
2026-06-16 16:19:31 +0100

    Lots of customisations

---
+++ files/scripts/1password-repo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo
