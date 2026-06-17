37878a4fb2cc8cc5a204afeb83ee9d1f43c0034c
Pete Goodall <pete@digicratic.com>
2026-06-17 10:50:57 +0100

    Fixed error with creating group and user

--- files/scripts/config-greetd.sh
+++ files/scripts/config-greetd.sh
@@ -6,7 +6,20 @@
 set -oue pipefail
 
 # Your code goes here.
-groupadd -r greetd
-useradd -r -g greetd -d /var/lib/greetd -s /sbin/nologin -c "greetd daemon user" greetd
+echo "Checking for the group: _greetd"
+if ![ id -g "_greetd" > /dev/null 2>&1 ]; then
+    #groupadd -r greetd;
+    echo "creating the group: greetd"
+fi
+
+echo "Checking for the user: _greetd"
+if ![ id -u "_greetd" > /dev/null 2>&1 ]; then
+    #useradd -r -g greetd -d /var/lib/greetd -s /sbin/nologin -c "greetd daemon user" greetd
+    echo "creating the user: greetd"
+fi
+
+# If this fails I want the script to fail
 chown -R greetd:greetd /etc/greetd/
+
+# This will no fail even if the user is already in the video group
 usermod -aG video greetd
